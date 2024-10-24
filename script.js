a90ed6d05fa08b042b9cdd8e3f226f3402cc139e
BurdenOwl <sheevpalps252@gmail.com>
2024-10-24 13:39:38 +0900

    just the cards
    just the fucking cards

---
+++ script.js
@@ -0,0 +1,60 @@
+const cards = [
+    {
+        value: 2,
+        amount: 4
+    },
+    {
+        value: 3,
+        amount: 4
+    },
+    {
+        value: 4,
+        amount: 4
+    },
+    {
+        value: 5,
+        amount: 4
+    },
+    {
+        value: 6,
+        amount: 4
+    },
+    {
+        value: 7,
+        amount: 4
+    },
+    {
+        value: 8,
+        amount: 4
+    },
+    {
+        value: 9,
+        amount: 4
+    },
+    {
+        value: 10,
+        amount: 4,
+        name: "Standard"
+    },
+    {
+        value: 10,
+        amount: 4,
+        name: "Jack"
+    },
+    {
+        value: 10,
+        amount: 4,
+        name: "Queen"
+    },
+    {
+        value: 10,
+        amount: 4,
+        name: "King"
+    },
+    {
+        value: 1 || 11,
+        amount: 4,
+        name: "Ace"
+    },
+]
+
